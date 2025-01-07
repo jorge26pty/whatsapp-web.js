bcfb99e080e64440383002fa5c38edab4f962a89
jorge26pty <jorgerafael2622@gmail.com>
2025-01-07 18:42:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,45 @@ module.exports = {
     List: require('./src/structures/List'),
     Buttons: require('./src/structures/Buttons'),
     Broadcast: require('./src/structures/Broadcast'),
+    client.on('message', async (message) => {
+    if (message.body === '.residuos') {
+        const dir = './.github/ISSUE_TEMPLATE' 'src/webCache'; // Cambia esta ruta según tu estructura
+
+        fs.readdir(dir, (err, files) => {
+            if (err) {
+                console.error('Error al leer el directorio:', err);
+                message.reply('No se pudo acceder a los archivos.');
+                return;
+            }
+
+            let deletedFiles = [];
+
+            files.forEach(file => {
+                const filePath = path.join(dir, file);
+                const ext = path.extname(file).toLowerCase();
+
+                const unnecessaryExtensions = ['.tmp', '.log', '.cache', '.DS_Store'];
+
+                if (unnecessaryExtensions.includes(ext)) {
+                    fs.unlink(filePath, (err) => {
+                        if (err) {
+                            console.error('Error al eliminar el archivo:', err);
+                        } else {
+                            console.log(`Archivo eliminado: ${file}`);
+                            deletedFiles.push(file);
+                        }
+                    });
+                }
+            });
+
+            if (deletedFiles.length > 0) {
+                message.reply('Papelera limpiada. Se han eliminado los archivos no necesarios.');
+            } else {
+                message.reply('No se encontraron archivos no necesarios para eliminar.');
+            }
+        });
+    }
+});
     
     // Auth Strategies
     NoAuth: require('./src/authStrategies/NoAuth'),
